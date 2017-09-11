75d8398e983ec58242bc3d44febb47ef84265e67
DESKTOP-POUBFF4\dimon <dimon_bc95@outlook.com>
2017-09-11 15:40:11 +0300

    Added style to clients component. Component styled with twitter bootstrap.

--- src/containers/clients.js
+++ src/containers/clients.js
@@ -1,3 +1,31 @@
-/**
- * Created by dimon on 11.09.2017.
- */
+import React from 'react'
+import {receiveClients, setName, changeCurrentPage} from '../actions/clients'
+import {connect} from 'react-redux'
+import Clients from '../components/Clients'
+import {bindActionCreators} from 'redux'
+
+class ClientsContainer extends React.Component {
+  componentWillMount() {
+    const page = this.props.match.params.page
+    if (!!page) {
+      this.props.receiveClients(page)
+      this.props.changeCurrentPage(+page)
+      return true
+    }
+
+    this.props.receiveClients(1)
+  }
+
+  render() {
+    return <Clients {...this.props}/>
+  }
+}
+
+const mapStateToProps = ({clients}) => ({
+  clients: clients.clientsList,
+  ...clients
+})
+
+const mapDispatchToProps = dispatch => bindActionCreators({onSubmit: setName, receiveClients, changeCurrentPage}, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(ClientsContainer)
