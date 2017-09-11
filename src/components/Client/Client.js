0ba0b17bd6c8fd41596115e5f30d1c3772d880f2
DESKTOP-POUBFF4\dimon <dimon_bc95@outlook.com>
2017-09-11 18:03:28 +0300

    Added loading and error to client component.

--- src/components/Client/Client.js
+++ src/components/Client/Client.js
@@ -6,21 +6,24 @@ const Client = ({clientInfo, isFetching, error}) => {
   return (
     <Grid>
       <PageHeader>Client</PageHeader>
-      <Row>
-        <Col lgOffset={4} mdOffset={4} smOffset={4} xsOffset={3}>
-          <ul className='clientInfo'>
-            <li className='clientInfoItem'>ID: {clientInfo.id}</li>
-            <li className='clientInfoItem'>Name: {clientInfo.name}</li>
-            <li className='clientInfoItem'>Gender: {clientInfo.gender}</li>
-            <li className='clientInfoItem'>
-              Birthday: {!!clientInfo.birthYear ? `${clientInfo.birthYear}.${clientInfo.birthMonth}.${clientInfo.birthDay}` : ' - '}</li>
-            <li className='clientInfoItem'>Phone: {clientInfo.contactPerson || ' - '}</li>
-            <li className='clientInfoItem'>Email: {clientInfo.email || ' - '}</li>
-            <li className='clientInfoItem'>Card number: {clientInfo.cardNumber || ' - '}</li>
-            <li className='clientInfoItem'>Address: {clientInfo.address || ' - '}</li>
-          </ul>
-        </Col>
-      </Row>
+      {isFetching ? <p className='loading'>Loading...</p> : (error ?
+        <p className='requestError'>ERROR: Can`t load clientInfo</p> : (
+        <Row>
+          <Col lgOffset={4} mdOffset={4} smOffset={4} xsOffset={3}>
+            <ul className='clientInfo'>
+              <li className='clientInfoItem'>ID: {clientInfo.id}</li>
+              <li className='clientInfoItem'>Name: {clientInfo.name}</li>
+              <li className='clientInfoItem'>Gender: {clientInfo.gender}</li>
+              <li className='clientInfoItem'>
+                Birthday: {!!clientInfo.birthYear ? `${clientInfo.birthYear}.${clientInfo.birthMonth}.${clientInfo.birthDay}` : ' - '}</li>
+              <li className='clientInfoItem'>Phone: {clientInfo.contactPerson || ' - '}</li>
+              <li className='clientInfoItem'>Email: {clientInfo.email || ' - '}</li>
+              <li className='clientInfoItem'>Card number: {clientInfo.cardNumber || ' - '}</li>
+              <li className='clientInfoItem'>Address: {clientInfo.address || ' - '}</li>
+            </ul>
+          </Col>
+        </Row>
+      ))}
     </Grid>
   )
 }
